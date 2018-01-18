be17d3d5446f361c3ee274fc866ead577bc6413d
Adam <Adam.Nagy@betssongroup.com>
2018-01-18 17:09:01 +0100

    console shell added, not finished

---
+++ source/Nadam.ConsoleShell/CommandModels/Command.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Nadam.ConsoleShell.CommandModels
+{
+    public class Command
+    {
+        public IList<string> Arguments { get; private set; }
+        public string Name { get; set; }
+        public string LibraryClassName { get; set; }
+        public Type Type { get; set; }
+        public bool IsStatic { get; set; }
+
+	    public Command()
+	    {
+		    
+	    }
+
+        public Command(string input)
+        {
+            // Ugly regex to split string on spaces, but preserve quoted text intact:
+            var stringArray = Regex.Split(input, "(?<=^[^\"]*(?:\"[^\"]*\"[^\"]*)*) (?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+
+            Arguments = new List<string>();
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                // The first element is always the command:
+                if (i == 0)
+                {
+                    this.Name = stringArray[i];
+
+                    // Set the default:
+                    this.LibraryClassName = "DefaultCommands";
+                    string[] s = stringArray[0].Split('.');
+                    if (s.Length == 2)
+                    {
+                        this.LibraryClassName = s[0];
+                        this.Name = s[1];
+                    }
+                }
+                else
+                {
+                    var inputArgument = stringArray[i];
+
+                    // Assume that most of the time, the input argument is NOT quoted text:
+                    string argument = inputArgument;
+
+                    // Is the argument a quoted text string?
+                    var regex = new Regex("\"(.*?)\"", RegexOptions.Singleline);
+                    var match = regex.Match(inputArgument);
+
+                    // If it IS quoted, there will be at least one capture:
+                    if (match.Captures.Count > 0)
+                    {
+                        // Get the unquoted text from within the qoutes:
+                        var captureQuotedText = new Regex("[^\"]*[^\"]");
+                        var quoted = captureQuotedText.Match(match.Captures[0].Value);
+
+                        // The argument should include all text from between the quotes
+                        // as a single string:
+                        argument = quoted.Captures[0].Value;
+                    }
+                    Arguments.Add(argument);
+                }
+            }
+        }
+    }
+}
---
+++ source/Nadam.ConsoleShell/ConsoleCommand/CommandManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Nadam.ConsoleShell.CommandModels;
+
+namespace Nadam.ConsoleShell.ConsoleCommand
+{
+	public class CommandManager
+	{
+		private readonly CommandLibrary commandLibrary;
+
+		public CommandManager()
+		{
+			var register = new CommandRegister("Nadam.ConsoleShell.DefaultCommands");
+			commandLibrary = register.RegisterCommands();
+		}
+
+		public Command BuildCommand(string input)
+		{
+			// Ugly regex to split string on spaces, but preserve quoted text intact:
+			var stringArray = Regex.Split(input, "(?<=^[^\"]*(?:\"[^\"]*\"[^\"]*)*) (?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+
+			// case 1: input is an alias name, does not contain dot (.)
+			if (stringArray[0].Contains('.'))
+			{
+				var classAndFunc = stringArray[0].Split('.');
+				var className = classAndFunc[0];
+				var function = classAndFunc[1];
+				var commandClass = commandLibrary.GetCommandClass(className);
+			}
+			// case 2: input is a full reference, contains <class-name>.<function-name>
+			else
+			{
+				var commandFunction = stringArray[0];
+			}
+			throw new NotImplementedException();
+		}
+
+		public string ExecuteCommand(Command command)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
