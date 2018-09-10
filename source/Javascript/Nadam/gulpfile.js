0cf0eb2c680713bb8565de723e42127553dfc39b
Adam <Adam.Nagy@betssongroup.com>
2018-09-10 09:53:52 +0200

    addding gulp rel and dev config

--- source/Javascript/Nadam/gulpfile.js
+++ source/Javascript/Nadam/gulpfile.js
@@ -1,13 +1,33 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
-var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
+var minify = require('gulp-minify');
+var pump = require('pump');
 
-gulp.task('concat-scripts', function(){
-	return gulp.src(['src/Nadam.Globals.*.js',
-					 'src/Nadam.Extensions.*.js'])
-		.pipe(concat('Nadam.JsLib.Index.js'))
-		.pipe(gulp.dest('dest/'));
-  });
+gulp.task('dev', function(cb) {
+    pump([
+            gulp.src(['src/Nadam.Globals.*.js',
+                'src/Nadam.Extensions.*.js'
+            ]),
+            concat('dev-Nadam.JsLib.Index.js'),
+            gulp.dest('./dest/')
+        ],
+        cb
+    );
+});
 
-  gulp.task('default');
+gulp.task('rel', function(cb) {
+    pump([
+            gulp.src(['src/Nadam.Globals.*.js',
+                'src/Nadam.Extensions.*.js'
+            ]),
+            concat('Nadam.JsLib.Index.js'),
+            uglify(),
+            minify(),
+            gulp.dest('./../../../dist/Javascript/')
+        ],
+        cb
+    );
+});
+
+gulp.task('default');
