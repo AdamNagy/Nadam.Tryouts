d851b51580363e7444fdd26ae9103b5868724b3a
Adam Nagy <adna01@betssongroup.com>
2019-05-03 11:50:26 +0200

    deleting and will be re-added

--- Angular/Angular-demo_event_manager/app/common/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './collapsible-well.component';
-export * from './jQuery.service';
-export * from './toastr.service';
-export * from './simpleModal.component';
-export * from './modalTrigger.directive';
