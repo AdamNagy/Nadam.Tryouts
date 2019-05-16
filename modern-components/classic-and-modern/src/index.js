28ab93b433348e01850389faca441e4951a9bc88
Adam Nagy <adna01@betssongroup.com>
2019-05-16 16:44:44 +0200

    es6 and webpack with classic libs and bootstrap

--- modern-components/classic-and-modern/src/index.js
+++ modern-components/classic-and-modern/src/index.js
@@ -1,6 +1,7 @@
-// import $ from "jquery";
+// import $ from "jquery";	// OPTION 1 to include jquery, other is in webpack.config
 import 'jquery-ui/ui/widgets/draggable';
 import 'jquery-ui/ui/widgets/resizable';
+
 
 (function() {
 
--- modern-components/classic-and-modern/webpack.config.js
+++ modern-components/classic-and-modern/webpack.config.js
@@ -2,13 +2,20 @@ const path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+		app: './src/index.js',
+		bootstrap: './src/bootstrap.js'
+	},
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/
+	  },
+	  {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
@@ -16,14 +23,21 @@ module.exports = {
     extensions: [ '.tsx', '.ts', '.js' ]
   },
   output: {
-    filename: 'main.js',
+	filename: '[name].bundle.js',
+	chunkFilename: '[name].chunk.js',
     path: path.resolve(__dirname, 'dist')
   },
+  // OPTION 2 to include jquery, other is in index.js
   plugins: [
 	new webpack.ProvidePlugin({
 		$: 'jquery',
 		jQuery: 'jquery',
 		'window.jQuery': 'jquery'
-    })
-  ]
+	})
+  ],
+  optimization: {
+		splitChunks: {
+		chunks: 'all'
+		}
+	}
 };
