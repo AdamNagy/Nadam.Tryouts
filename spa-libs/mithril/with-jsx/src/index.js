fde71ec4790ad341a9a2d403386efacebf1a8da7
Adam Nagy <adna01@betssongroup.com>
2019-06-17 17:06:37 +0200

    add unsaved change

---
+++ spa-libs/mithril/with-jsx/src/index.js
@@ -0,0 +1,10 @@
+import m from "mithril";
+import { MyJSXComponent } from "./my-component/my-jsx.component";
+import { StandardComponent } from "./my-component/standard.component";
+import { CustomElements } from "./custom-elements/index";
+
+CustomElements.Init();
+m.render(document.body, "hello world");
+var myComp = new StandardComponent(document.body);
+myComp.Render();
+m.mount(document.body, MyJSXComponent);
