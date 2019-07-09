7885cb4f7ed9f6bb1593abd362df2a5bcaa690c4
AdamNagy <nagy.quest@gmail.com>
2019-07-09 22:50:16 +0200

    empty test for linq to folder system

---
+++ DotNet/CshTryouts/CustomQueryable.Tests/LinqToFolderSystemTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace CustomQueryable.Tests
+{
+    [TestClass]
+    public class LinqToFolderSystemTests
+    {
+        [TestMethod]
+        public void Test1()
+        {
+
+        }
+    }
+}
