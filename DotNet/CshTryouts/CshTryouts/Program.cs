3034721b98afa4c344d06a5f19b7065c8b3bd189
Adam Nagy <adna01@betssongroup.com>
2019-07-09 13:25:26 +0200

    adding notes for learning, rename folder in csh tryouts

--- DotNet/CshTryouts/CshTryouts/Program.cs
+++ DotNet/CshTryouts/CshTryouts/Program.cs
@@ -7,14 +7,12 @@ namespace CshTryouts
     {
         static void Main(string[] args)
         {
-            foreach (var fibn in YieldUseage.Fibonacci(100))
+            foreach (var fibn in YieldUseage.Fibonacci(10))
             {
                 Console.Write($"{fibn} ");
             }
 
             Console.ReadKey();
         }
-
-
     }
 }
