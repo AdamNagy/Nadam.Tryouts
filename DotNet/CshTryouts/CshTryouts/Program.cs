ffcaae8a9ed5104ca1e554c4f9fd902dcca1bffc
Adam Nagy <adna01@betssongroup.com>
2019-07-12 11:40:48 +0200

    new test file searcher like an util

--- DotNet/CshTryouts/CshTryouts/Program.cs
+++ DotNet/CshTryouts/CshTryouts/Program.cs
@@ -12,7 +12,52 @@ namespace CshTryouts
                 Console.Write($"{fibn} ");
             }
 
-            Console.ReadKey();
+            ValueAnRefTypeDemo();
+
+            // Console.ReadKey();
         }
+
+        static void ValueAnRefTypeDemo()
+        {
+            var myNum = 5;
+            ValueTypeAsRef_ChangeValue(ref myNum);
+
+            var adam = new Person(){Name = "Adam"};
+            RefTypeWithoutRef_ChangingSomeProp(adam);
+
+            RefTypeWithRef_ChangingSomeProp(ref adam);
+            RefTypeWithRef_ChangingEntireObj(ref adam);
+            RefTypeWithoutRef_ChangingEntireObj(adam);
+        }
+
+        static void ValueTypeAsRef_ChangeValue(ref int value)
+        {
+            value++;
+        }
+
+        static void RefTypeWithoutRef_ChangingSomeProp(Person myObj)
+        {
+            myObj.Name = "Adam2";
+        }
+
+        static void RefTypeWithRef_ChangingSomeProp(ref Person myObj)
+        {
+            myObj.Name = "Adam3";
+        }
+
+        static void RefTypeWithRef_ChangingEntireObj(ref Person myObj)
+        {
+            myObj = new Person(){ Name = "Johy" };
+        }
+
+        static void RefTypeWithoutRef_ChangingEntireObj(Person myObj)
+        {
+            myObj = new Person() { Name = "Wick" };
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
     }
 }
---
+++ DotNet/MyUtils/MyUtils/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyUtils
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Base folder is: {args[0]}");
+            var util = new MyUtils(args[0], "SportsbookTestingModule");
+            util.Process();
+
+            Console.WriteLine("\nProcess is successful!\nPress any key to exit..");
+            Console.ReadKey();
+        }
+    }
+
+    public class MyUtils
+    {
+        public string BaseFolder { get; private set; }
+        public string SearchTerm { get; private set; }
+
+        private List<string> files;
+        private Dictionary<string, bool> result;
+
+        public MyUtils(string baseFolder, string searchTerm)
+        {
+            BaseFolder = baseFolder;
+            SearchTerm = searchTerm;
+
+            files = new List<string>();
+            result = new Dictionary<string, bool>();
+        }
+
+        public bool Process()
+        {
+            try
+            {
+                SearchForFiles("", IsIntegrationTestFile);
+                foreach (var file in files)
+                    result.Add(file, ProcessFile($"{BaseFolder}\\{file}"));
+
+                LogResult();
+                return false;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+        private void LogResult()
+        {
+            Console.WriteLine("The result is: ");
+            Console.WriteLine($"Number of files processed: {files.Count}");
+            foreach (var result in result)
+                Console.WriteLine($"{result.Key, -100}: {result.Value}");
+        }
+
+        #region Folder and file filtering
+        private void SearchForFiles(string relativePath, Func<string, bool> pred)
+        {
+            var currentDir = $"{BaseFolder}{relativePath}";
+
+            foreach (var file in GetFiles(currentDir, pred))
+                files.Add($"{relativePath}\\{file}");
+
+            var subFolders = Directory.EnumerateDirectories(currentDir)
+                .Select(p => p.Split('\\').Last());
+            foreach (var subDir in subFolders)
+            {
+                if (string.IsNullOrEmpty(relativePath))
+                    SearchForFiles($"\\{subDir}", pred);
+                else
+                    SearchForFiles($"\\{relativePath}\\{subDir}", pred);
+
+            }
+        }
+
+        private List<string> GetFiles(string folder, Func<string, bool> pred)
+            => Directory.GetFiles(folder)
+                .Select(Path.GetFileName)
+                .Where(pred)
+                .ToList();
+
+        private bool IsIntegrationTestFile(string fileName)
+        {
+            if (   fileName.EndsWith("effect.spec.ts")
+                || fileName.EndsWith("action.spec.ts")
+                || fileName.EndsWith("reducer.spec.ts")
+                || fileName.EndsWith("selector.spec.ts")
+                || fileName.EndsWith("effect.spec.ts")
+                || fileName.EndsWith("model.spec.ts")
+                || fileName.EndsWith("util.spec.ts")
+                || fileName.EndsWith("utils.spec.ts")
+                || fileName.EndsWith("service.spec.ts")
+                || fileName.EndsWith("guard.spec.ts")
+                || fileName.EndsWith("pipe.spec.ts")
+                || !fileName.EndsWith(".spec.ts")
+                )
+            {
+                return false;
+            }
+
+            return true;
+        }
+        #endregion
+
+        #region File processing
+        private bool ProcessFile(string path)
+        {
+            var file = new StreamReader(path);
+            string line;
+            while ((line = file.ReadLine()) != null)
+            {
+                if( IsLineIrrelevant(line) )
+                    continue;
+
+                if (line.Contains(SearchTerm))
+                    return true;
+            }
+
+            file.Close();
+            return false;
+        }
+
+        private bool IsLineIrrelevant(string line)
+        {
+            if (line.StartsWith("import")
+                || line.StartsWith("}")
+                || line.StartsWith("describe"))
+            {
+                return true;
+            }
+
+            return false;
+        }
+        #endregion
+
+    }
+}
