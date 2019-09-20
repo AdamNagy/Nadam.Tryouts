5d4a3ee3dfc7b93fa7f7ae6722b1a90eedb960f5
Adam Nagy <adna01@betssongroup.com>
2019-09-20 17:10:59 +0200

    adding .net core poc

---
+++ DotNet/TechPoc/POC_DotNETCore/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace POC_DotNETCore
+{
+    class Program
+    {
+        public static ServiceProvider container;
+        public static IConfigurationRoot config;
+
+        static void Main(string[] args)
+        {
+            container = new ServiceCollection()
+                .AddScoped<App>()
+                .AddScoped<Dependency>()
+                .BuildServiceProvider();
+
+            container.GetService<App>()
+                .Run();
+
+            config = new ConfigurationBuilder()
+                 // .SetBasePath(Directory.GetCurrentDirectory())
+                 .AddJsonFile("appsettings.json")
+                 .Build();
+        }
+    }
+
+    public class App
+    {
+        public Dependency Dependency { get; }
+
+        public void Run()
+        {
+            Console.WriteLine("Hello Core");
+        }
+
+        public App(Dependency dependency)
+        {
+            Dependency = dependency;
+        }
+    }
+
+    public class Dependency
+    {
+
+    }
+}
