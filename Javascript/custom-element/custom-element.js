5fed35893f23e1f2d8c8f2a5af03ae9956cefce6
Adam Nagy <adna01@betssongroup.com>
2019-10-16 14:46:37 +0200

    custom element playaround part 2

--- Javascript/custom-element/custom-element.js
+++ Javascript/custom-element/custom-element.js
@@ -1,38 +1,36 @@
 class CustomElement extends HTMLElement {
 
-	get text() {
-		return this.getAttribute('text');
-	}
+	// get text() {
+		// return this.getAttribute('text');
+	// }
 
-	set text(newText) {
-		this.wrapper.innerHTML = this.text;
-		this.setAttribute('value', newText);
-	}
+	// set text(newText) {
+		// this.wrapper.innerHTML = this.text;
+		// this.setAttribute('value', newText);
+	// }
 
 	static get observedAttributes() {
 		return ['text'];
-	}
+	} 
 
 	wrapper = document.createElement('span');
+	templateId = "template_custom-element";
 
 	constructor() {
 		super();
     
-		console.log("custom element ctor");
-		var shadow = this.attachShadow({mode: 'open'});
-		 
-		if(this.hasAttribute('text')) {
-			this.wrapper.innerHTML = this.text;
-		}
-
-		shadow.appendChild(this.wrapper);
+		const template = document.getElementById(this.templateId);
+		const node = document.importNode(template.content, true);
+	
+		var shadow = this.attachShadow({mode: 'open'});	 
+		shadow.appendChild(node);
 	}
 	
-	attributeChangedCallback(name, oldValue, newValue) {
-		console.log(`${oldValue} - ${newValue}`);
-		this[name] = newValue;
-		this.wrapper.innerHTML = this.text;
-	}
+	// attributeChangedCallback(name, oldValue, newValue) {
+		// console.log(`${oldValue} - ${newValue}`);
+		// this[name] = newValue;
+		// this.wrapper.innerHTML = this.text;
+	// }
 }
 
 customElements.define('custom-element', CustomElement);
