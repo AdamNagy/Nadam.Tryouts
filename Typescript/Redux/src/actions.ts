1fb894788d959c34c2cae1c0745ff9a047dda9be
Adam Nagy <adna01@betssongroup.com>
2019-11-19 11:12:35 +0100

    redux basic demo

---
+++ Typescript/Redux/src/actions.ts
@@ -0,0 +1,42 @@
+/*
+ * action types
+ */
+
+export const ADD_TODO = "ADD_TODO";
+export const TOGGLE_TODO = "TOGGLE_TODO";
+export const SET_VISIBILITY_FILTER = "SET_VISIBILITY_FILTER";
+
+/*
+ * other constants
+ */
+
+export const VisibilityFilters = {
+	SHOW_ACTIVE: "SHOW_ACTIVE",
+	SHOW_ALL: "SHOW_ALL",
+	SHOW_COMPLETED: "SHOW_COMPLETED",
+};
+
+/*
+ * action creators
+ */
+
+export function addTodo(text: string) {
+	return {
+		text,
+		type: ADD_TODO,
+	};
+}
+
+export function toggleTodo(index: number) {
+	return {
+		index,
+		type: TOGGLE_TODO,
+	};
+}
+
+export function setVisibilityFilter(filter: string) {
+	return {
+		filter,
+		type: SET_VISIBILITY_FILTER,
+	};
+}
--- Typescript/Redux/src/index.ts
+++ Typescript/Redux/src/index.ts
@@ -1,15 +1,4 @@
 import { createStore } from "redux";
+import todoApp from "./reducers";
 
-function counter(state = 0, action: any) {
-	switch (action.type) {
-		case "INCREMENT":
-			return state + 1;
-		case "DECREMENT":
-			return state - 1;
-		default:
-			return state;
-	}
-}
-
-const store = createStore(counter);
-(window as any).store = store;
+export const store = createStore(todoApp);
