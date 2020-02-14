44012b29039d9d203e234ca8472ec93b812243f3
adna01 <adam.nagy@betsssongroup.com>
2020-02-14 15:42:17 +0100

    playing with graph staffs

--- DotNet/CshTryouts/Graphs/Graph/IGraph.cs
+++ DotNet/CshTryouts/Graphs/Graph/IGraph.cs
@@ -10,15 +10,13 @@ namespace Graphs.Graph
 		bool Contains(TNode nodeValue);
 		bool Contains(TNode a, TNode b);
 
-		Node<TNode> FindNode(TNode reference);
-		Edge FindEdge(TNode a, TNode b);
+		TNode FindNode(TNode reference);
+		// Edge FindEdge(TNode a, TNode b);
 
 		bool Remove(TNode nodeValue);
-		bool Remove(Node<TNode> node);
-		bool Remove(Edge edge);
 	    bool Remove(TNode a, TNode b);
 
-        IEnumerator<Node<TNode>> Nodes();
-		IEnumerator<Edge> Edges();
+        IEnumerator<TNode> Nodes();
+		// IEnumerator<Edge> Edges();
 	}
 }
---
+++ DotNet/CshTryouts/Graphs/Trees/BinaryTree/NDimTree.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs.Trees.BinaryTree
+{
+    public class NDimTreeNode<TNode>
+    {
+        protected NDimTreeNode<TNode>[] children;
+
+        public NDimTreeNode(int dim)
+        {
+            children = new NDimTreeNode<TNode>[dim];
+        }
+    }
+
+    public class TwoDimensionalTree<TNode> : NDimTreeNode<TNode>
+    {
+        public TwoDimensionalTree<TNode> left
+        {
+            get => (TwoDimensionalTree<TNode>) children[0];
+        }
+
+        public TwoDimensionalTree<TNode> right
+        {
+            get => (TwoDimensionalTree<TNode>)children[1];
+        }
+
+        public TwoDimensionalTree() : base(2) { }
+        
+        /// <summary>
+        /// the function that sorts the children
+        /// For 2 dimensinal tree (binary tree) this can be the 'less than' predicate which has 2 state like boolean
+        /// </summary>
+        protected Func<TNode, TNode, bool> sorter;
+
+        public IEnumerable<TwoDimensionalTree<TNode>> InOrder(TwoDimensionalTree<TNode> root = null)
+        {
+            if( root == null )
+                yield return this;
+
+            foreach (var child in children)
+            {
+                foreach (var grand in InOrder((TwoDimensionalTree<TNode>) child))
+                    yield return grand;
+            }
+        }
+    }
+
+
+}
