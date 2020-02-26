1d0023558470f4d9ede7bbb06d2c70ab3911c8a5
adna01 <adam.nagy@betsssongroup.com>
2020-02-26 11:56:14 +0100

    fine tune scraper base controller

--- DotNet/POC/Framework_ScraperDemo/Framework_ScraperDemo/Controllers/HomeController.cs
+++ DotNet/POC/Framework_ScraperDemo/Framework_ScraperDemo/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace Framework_ScraperDemo.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index([FromUri]string url = "https://www.w3school.com")
+        public ActionResult Index([FromUri]string url = "https://www.w3schools.com/tags/tag_a.asp")
         {
             var web = new HtmlWeb();
             var doc = web.Load(url, "GET");
@@ -17,7 +17,7 @@ namespace Framework_ScraperDemo.Controllers
             RemoveElemetsByName(ref doc, "iframe");
 
             MoveHeaderStylesToBody(ref doc);
-            HackLinkTags(ref doc);
+            HackAnchorTags(ref doc);
 
             var viewModel = new ScraperEchoViewModel(
                 doc.DocumentNode
@@ -42,14 +42,29 @@ namespace Framework_ScraperDemo.Controllers
 
         private void MoveHeaderStylesToBody(ref HtmlDocument htmlDoc)
         {
+            var body = htmlDoc.DocumentNode.SelectSingleNode("//body");
             var styleNodes = htmlDoc.DocumentNode.SelectNodes("//head/style")
                 .Select(p => HtmlNode.CreateNode($"<style>{p.InnerText}</style>"));
 
             foreach (var styleNode in styleNodes)
-                htmlDoc.DocumentNode.SelectSingleNode("//body").AppendChild(styleNode);
+                body.AppendChild(styleNode);
+
+            var styleLinkNodes = htmlDoc.DocumentNode.SelectNodes("//head/link[@rel='stylesheet']")
+                .Select(p => HtmlNode.CreateNode($"<link type='text/css' rel='stylesheet' href='{ExpandLinkWithDomain(p.Attributes["href"].Value)}'>"));
+
+            foreach (var styleLinkNode in styleLinkNodes)
+                body.AppendChild(styleLinkNode);
         }
 
-        private void HackLinkTags(ref HtmlDocument htmlDoc)
+        private string ExpandLinkWithDomain(string url)
+        {
+            if (url.StartsWith("http"))
+                return url;
+
+            return $"https://www.w3schools.com/{url}";
+        }
+
+        private void HackAnchorTags(ref HtmlDocument htmlDoc)
         {
             var linkTags = htmlDoc.DocumentNode.SelectNodes($"//a");
             if (linkTags == null)
