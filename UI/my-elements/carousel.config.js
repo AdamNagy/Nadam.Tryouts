6510de39f06a29cc2876a928e3953bbf8d1c5256
adna01 <adam.nagy@betsssongroup.com>
2020-06-16 21:49:17 +0200

    adding some test images, and fine tune carousel

---
+++ UI/my-elements/carousel.config.js
@@ -0,0 +1,94 @@
+// Multiple slides carousel config
+class SingleSlideCarouselConfig {
+	dots = true;
+	arrows = false;
+	infinite = true;
+	speed = 300;
+	slidesToShow = 1; 
+	slidesToScroll = 1;
+	autoplay = false;
+	autoplaySpeed = 3000;
+	lazyLoad = "ondemand";
+	responsive = [
+		{
+			breakpoint: 1200,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+			}
+		},
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+			}
+		},
+		{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				infinite: true,
+				dots: true
+			}
+		},
+		{
+			breakpoint: 576,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				infinite: true,
+				dots: true
+			}
+		}
+	]
+}
+
+// Multiple slides carousel config
+class MultiSlideCarouselConfig {
+	dots = true;
+	arrows = false;
+	infinite = true;
+	speed = 300;
+	slidesToShow = 4; 
+	slidesToScroll = 3;
+	autoplay = false;
+	autoplaySpeed = 3000;
+	variableWidth = true;
+	lazyLoad = "ondemand";
+	responsive = [
+		{
+			breakpoint: 1200,
+			settings: {
+				slidesToShow: 4,
+				slidesToScroll: 3,
+			}
+		},
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 2,
+			}
+		},
+		{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 2,
+				infinite: true,
+				dots: true
+			}
+		},
+		{
+			breakpoint: 576,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				infinite: true,
+				dots: true
+			}
+		}
+	]
+}
