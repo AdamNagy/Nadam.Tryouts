06593d234a7d1c51d85791d87c5e66f2db4993dd
adna01 <adam.nagy@betsssongroup.com>
2020-07-07 11:48:20 +0200

    ndm factory method requirements clarified

--- Javascript/classic-js/ndm-js/src/core/factory.js
+++ Javascript/classic-js/ndm-js/src/core/factory.js
@@ -1,10 +1,15 @@
 /*
 usage:
 ndm("default:div.container") returns <div class="container"></div>
+	. -> class
+	# -> id
+	* -> nid
+	(space) -> child
+	[attribute name = attrbiute value] -> attribute
+	{key} -> adds second patameters (config) 'key' named property as object to ctor as parameter
 ndm(custom:parallax#my-parallax.some-class, {...}) returns <ndm-parallax class="some-class" id="my-parallax">...</ndm-parallax>
 ndm("<div><p></p><img><img><section></section></div>...") returns <div><p></p><img><img><section></section></div>
 ndm("template:some-template-id") returns instantiated html document
-ndm("url:https://some.domain.com/custom-element.html") returns instantiated html document
 */
 
 function ndm(param, config) {
