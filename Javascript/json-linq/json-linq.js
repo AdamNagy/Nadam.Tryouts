08d38473ed7c06f0334b426fd0246da96fcb485b
adna01 <adam.nagy@betsssongroup.com>
2020-07-14 14:16:34 +0200

    css animation clean-up, json linq playaround

--- Javascript/json-linq/json-linq.js
+++ Javascript/json-linq/json-linq.js
@@ -26,29 +26,48 @@ function isIndexedArray(someArray) {
 }
 
 function trimChar(string, charToRemove) {
-    while(string.charAt(0)==charToRemove) {
-        string = string.substring(1);
-    }
 
-    while(string.charAt(string.length-1)==charToRemove) {
-        string = string.substring(0,string.length-1);
-    }
+	while(string.charAt(0) == charToRemove) {
+		string = string.substring(1);
+	}
 
-    return string;
+	while(string.charAt(string.length - 1) == charToRemove) {
+		string = string.substring(0, string.length - 1);
+	}
+
+	return string;
 }
 
 class JsonLinq {
 	
-	static select(propName, jsonObj, recursive) {
+	static select(propName, jsonObj) {
 
-		if( jsonObj === null || jsonObj === undefined)
+		if( jsonObj === null || jsonObj === undefined) {
 			return undefined;
-
-		if( recursive === undefined ) {
-			recursive = true;
 		}
 
-		var found = {}; 
+		if( propName === undefined || propName === null ) {
+			return undefined;
+		}
+
+		var found; 
+
+		// if property name contains dot (.) then it means it has to be solitted and search then one-by-one
+		// example select: prop1.prop2.prop3 -> prop3 has to be contained by prop2, and prop2 has to be contained by prop1
+		var propNames = propName.split('.');
+		if( propNames.length > 1 ) {
+
+			var propIdx = 0;
+			var fatherProp = JsonLinq.select(propNames[propIdx], jsonObj);
+			while( fatherProp !== undefined ) {
+				++propIdx;
+				fatherProp = JsonLinq.select(propNames[propIdx], fatherProp);
+
+				found = fatherProp || found;
+			}
+
+			return found;
+		}	
 
 		// primitive: number, string, boolean
 		if( isPrimitive(jsonObj) ) {
@@ -77,10 +96,9 @@ class JsonLinq {
 			}
 	
 			// go deeper
-			for(var prop in jsonObj) {
-	
-				if( recursive )
-					found = JsonLinq.select(propName, jsonObj[prop]);
+			for(var prop in jsonObj) {	
+				
+				found = JsonLinq.select(propName, jsonObj[prop]);
 
 				if( found ) {
 					return found;
@@ -100,13 +118,36 @@ class JsonLinq {
 		return jsonArray.filter(item => item[propName] === pred);
 	}
 
-	// example
-	// select:markets; where:eventId=f-ED0cC15OhkORmVTK2V2txg
+	// example: project: {}
+	static project(projectionRule, jsonObj) {
+		
+		var projection;
+		var projectorFunc = JsonLinq.createProjector(projectionRule);
+
+		if( isObject(jsonObj) ) {
+
+			projection = projectorFunc(jsonObj);
+
+		} else if( Array.isArray(jsonObj) && jsonObj.length > 0) {
+
+			projection = [];
+
+			for(var item of jsonObj) {
+				projection.push(projectorFunc(item));
+			}
+		}
+
+		return projection;
+	}
+
+	// example: select:markets; where:eventId=f-ED0cC15OhkORmVTK2V2txg; project: {}
 	static query(query, jsonObj) {
 
 		if( query === undefined || query === ""
-			|| jsonObj === null || jsonObj === undefined )
+			|| jsonObj === null || jsonObj === undefined ) 
+		{
 			return undefined;
+		}
 
 		query = trimChar(query.trim(), ";");
 		var instructions = query.split(';');
@@ -117,19 +158,20 @@ class JsonLinq {
 		for( var instruction of instructions ) {
 
 			var parsedInstruction = instruction.split(':');
-			if( parsedInstruction.length < 2 )
+			if( parsedInstruction.length < 2 ) {
 				throw `Invalid instruction: ${instruction}`;
+			}
 
 			var queryCommand = parsedInstruction[0].trim();
 			var commandParameter = parsedInstruction[1].trim();
 
+			var querySubject = resultState[resultState.length - 1];
+			if( !querySubject )
+				throw "query subject is undefined";
+
 			switch( queryCommand ) {
-				case "select":
-					var queryState = resultState[resultState.length - 1];
-					if( !queryState )
-						throw "query state is undefined";
-						
-					var result = JsonLinq.select(commandParameter, queryState);
+				case "select":						
+					var result = JsonLinq.select(commandParameter, querySubject);
 
 					if( result !== undefined ) {
 						resultState.push(result);
@@ -138,20 +180,23 @@ class JsonLinq {
 					continue;
 
 				case "where":
-					var queryState = resultState[resultState.length - 1];
-
-					if( !queryState )
-						throw "query state is undefined";
-
-					if( !Array.isArray(queryState) )
-						throw `json object is not an array, but query command is "where" which require an array`
+					if( !Array.isArray(querySubject) )
+						throw `json object is not an array, query command "where" require an array`
 
 					var whereParams = commandParameter.split('=').map(item => item.trim()).map(item => trimChar(item, "\"")).map(item => trimChar(item, "\'"));
-					var result = JsonLinq.where(whereParams[0], whereParams[1], queryState);
+					var result = JsonLinq.where(whereParams[0], whereParams[1], querySubject);
 					if( result !== undefined ) {
 						resultState.push(result);
 					}
 
+					continue;
+
+				case "project":
+					var result = JsonLinq.project(commandParameter, querySubject);
+					if( result !== undefined ) {
+						resultState.push(result);
+					}
+					
 					continue;
 
 				default: throw `Unkown query command: ${queryCommand}`;
@@ -159,5 +204,41 @@ class JsonLinq {
 		}
 
 		return resultState[resultState.length - 1]
+	}
+
+	static createProjector(params) {
+
+		var domain;
+
+		try{
+			// "{ newProp1: propertyOfTheInputItem-1, newProp2: propertyOfTheInputItem-2, ... }"
+			// or
+			// // "[ propertyOfTheInputItem-1, propertyOfTheInputItem-2, ... ]"
+			domain = JSON.parse( params );
+
+			return function(item) {
+
+				var projection = {};
+	
+				if(Array.isArray(domain)) {
+
+					var propertyIndex = 0;
+					for( var prop of domain ) {
+						projection[`p${++propertyIndex}`] = item[prop];
+					}
+
+				} else {
+
+					for( var prop in domain ) {
+						projection[prop]= item[domain[prop]];
+					}
+
+				}
+	
+				return projection;
+			}
+		} catch {
+			throw `Could not generate projector function based on the given data: ${params}`;
+		}
 	}
 }
