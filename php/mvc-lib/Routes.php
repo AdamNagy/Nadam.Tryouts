ccab091170bb2793e6928e274817d03f41b7aadd
AdamNagy <nagy.quest@gmail.com>
2020-07-28 09:33:59 +0200

    php mwc lib

--- php/mvc-lib/Routes.php
+++ php/mvc-lib/Routes.php
@@ -1,21 +1,18 @@
 <?php 
 
 	$url = explode('/', $_GET['url']);
+	$controller = "";
+	$action = "";
+	
+
 	print_r($url);
 
-	Route::set('about-us', function() {
-	
-		$controller = new AboutUs();
 
-		if( isset(url[2]) ) {
-			$controller->{$url[1]}($url[2]);
-		} else if (isset(url[1])) {
-			$controller->{$url[1]};
-		}
-	});
+	if( isset(url[2]) ) {
+		$controller->{$url[1]}($url[2]);
+	} else if (isset(url[1])) {
+		$controller->{$url[1]};
+	}
 
-	Route::set('contact-us', function() {
-		echo 'contact us';
-	});
 
 ?>
