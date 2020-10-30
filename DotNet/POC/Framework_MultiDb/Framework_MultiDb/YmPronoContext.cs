15eb11dfcd2fd1639d18e144c2aa53cb784a021f
adna01 <adam.nagy@betsssongroup.com>
2020-10-30 17:29:17 +0100

    initializing new poc project for entity framework with mysql

---
+++ DotNet/POC/Framework_MultiDb/Framework_MultiDb/YmPronoContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MultiDb.ImageModels;
+using MySql.Data.EntityFramework;
+
+namespace MultiDb
+{
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    public class YmPronoContext : DbContext
+    {
+        public DbSet<WebGallery> WebGalleries { get; set; }
+        public DbSet<StoredGallery> StoredGalleries { get; set; }
+        public DbSet<WebImage> WebImages { get; set; }
+        public DbSet<StoredImage> StoredImages { get; set; }
+
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<HibridImage> HibridImages { get; set; }
+
+        public YmPronoContext(string ctorString) : base(ctorString)
+        {
+            
+        }
+
+        public YmPronoContext(DbConnection existingConnection, bool contextOwnsConnection)
+            : base(existingConnection, contextOwnsConnection)
+        {
+            
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Write Fluent API configurations here
+
+        }
+    }
+}
