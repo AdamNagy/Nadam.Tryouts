13587105b408b7a9483faf504cde0ab6da61a0c3
adna01 <adam.nagy@betsssongroup.com>
2021-02-23 14:59:39 +0100

    new project Azure Cognitive Search demo

---
+++ DotNet/SchTest/Test2/Data/ICognitiveSearchService.cs
@@ -0,0 +1,12 @@
+﻿using Azure.Search.Documents.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Test2.Data
+{
+    public interface ICognitiveSearchService
+    {
+        public Task<IndexDocumentsResult> UpdateIndex();
+        public IEnumerable<GitApiModel> Search(string searchTerm);
+    }
+}
