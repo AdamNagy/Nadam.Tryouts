373ee1a0af106c7c81f857497c046ef5239233a8
nagy.quest@gmail.com <nagy.quest@gmail.com>
2022-05-20 17:30:44 +0200

    multithreading play skeleton

---
+++ DotNet/Multithreading/Multithreading/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Multithreading;
+
+var testDataGenerator = new TestDataGenerator(
+    @"C:\Users\Adam_Nagy1\Documents",
+    "test-urls.txt",
+    "test-data");
+
+testDataGenerator.GenerateTestFile();
