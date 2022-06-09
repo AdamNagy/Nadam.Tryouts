af486985c18317439b2cbd3d47e23558c376a282
nagy.quest@gmail.com <nagy.quest@gmail.com>
2022-06-09 08:40:38 +0200

    saving

---
+++ DotNet/POC/POC-DotNET-6/POC-DotNET-6/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using POC_DotNET_6.Models;
+
+namespace POC_DotNET_6.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<NationalPark> NationalParks { get; set; }
+        //public DbSet<Trail> Trails { get; set; }
+        //public DbSet<User> Users { get; set; }
+    }
+}
--- DotNet/POC/POC-DotNET-6/POC-DotNET-6/Program.cs
+++ DotNet/POC/POC-DotNET-6/POC-DotNET-6/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using POC_DotNET_6.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
