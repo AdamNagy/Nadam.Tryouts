2e05075893422edfa2b5f65c8e76379048412fb2
Adam Nagy <anagy@diligent.com>
2025-07-18 13:28:20 +0200

    fix auto height

--- UI/web-components/apps/ng-web-components-demo/src/app/components/auto-height/auto-height.directive.ts
+++ UI/web-components/apps/ng-web-components-demo/src/app/components/auto-height/auto-height.directive.ts
@@ -8,7 +8,7 @@ import {
   Input,
   Provider,
 } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 
 export const VIEW_STATE = new InjectionToken<ViewState>('VIEW_STATE');
 
@@ -21,27 +21,37 @@ export function provideViewState(): Provider {
   standalone: true,
 })
 export class AutoHeightDirective implements AfterViewInit {
-  private readonly viewState?: ViewState = inject(ViewState);
+  private readonly viewState: ViewState = inject(ViewState);
+  private readonly elementRef: ElementRef<HTMLElement> = inject(ElementRef);
 
-  constructor(private readonly elementRef?: ElementRef) {}
+  @Input('appAutoHeight') public hostId?: string | undefined;
 
   ngAfterViewInit(): void {
-    // this.viewState?.availableHeight$.subscribe((height) => {
-    // });
     if (this.elementRef?.nativeElement) {
-      const siblings = Array.from(
-        this.elementRef?.nativeElement.parentElement
-          ?.children as HTMLCollectionOf<HTMLElement>
-      );
-      let availHeight = this.viewState?.availableHeight || 0;
-      for (const sibling of siblings) {
-        // clientHeight includes padding but not margin, so we need to subtract margin
-        availHeight -= sibling.clientHeight;
-      }
-      //this.viewState!.availableHeight = availHeight;
-      // Set the height of the element to the available height.
-      // if there is any padding, then need to subtract additional 3 from availHeight
-      this.elementRef.nativeElement.style.height = `${availHeight - 3}px`;
+      const availHeight$ = this.viewState?.getAvailableHeight(this.hostId);
+      availHeight$?.subscribe((availHeight) => {
+        const siblings = Array.from(
+          this.elementRef?.nativeElement.parentElement
+            ?.children as HTMLCollectionOf<HTMLElement>
+        );
+        for (const sibling of siblings) {
+          if (sibling === this.elementRef.nativeElement) {
+            continue; // Skip the current element
+          }
+          // clientHeight includes padding but not margin, so we need to subtract margin
+          availHeight -= sibling.clientHeight;
+        }
+
+        const id = this.elementRef?.nativeElement.getAttribute('id');
+        if (id) {
+          this.viewState?.addChildHostHeight(id, availHeight);
+        }
+        //this.viewState!.availableHeight = availHeight;
+
+        // Set the height of the element to the available height.
+        // if there is any padding, then need to subtract additional 3 from availHeight
+        this.elementRef.nativeElement.style.height = `${availHeight - 3}px`;
+      });
     }
   }
 }
@@ -51,6 +61,7 @@ export class AutoHeightDirective implements AfterViewInit {
 })
 export class ViewState {
   #availableHeight = 0;
+  #childHostHeights: Map<string, BehaviorSubject<number>> = new Map();
 
   public get availableHeight(): number {
     return this.#availableHeight;
@@ -64,5 +75,30 @@ export class ViewState {
   #availableHeight$ = new BehaviorSubject<number>(0);
   public get availableHeight$(): Observable<number> {
     return this.#availableHeight$.asObservable();
+  }
+
+  public getAvailableHeight(id?: string): Observable<number> {
+    if (!id) {
+      return of(this.#availableHeight);
+    }
+
+    const exist = this.#childHostHeights.get(id);
+    if (exist) {
+      return exist.asObservable();
+    }
+
+    const heightSubject = new BehaviorSubject<number>(this.#availableHeight);
+    this.#childHostHeights.set(id, heightSubject);
+    return heightSubject.asObservable();
+  }
+
+  public addChildHostHeight(id: string, height: number): void {
+    const existing = this.#childHostHeights.get(id);
+
+    if (existing) {
+      existing.next(height);
+    } else {
+      this.#childHostHeights.set(id, new BehaviorSubject(height));
+    }
   }
 }
